1aabef5bce658e2b82d58e199fbb7d51fbe85a3b
Punit Bathija <punit644@gmail.com>
2022-12-26 13:05:56 +0530

    learnt how to write documentation for routes

--- index.js
+++ index.js
@@ -7,9 +7,31 @@ const PORT = 4000;
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.get("/", (req, res) => {
+let courses = [
+  {
+    id: "11",
+    name: "Learn React JS",
+    price: 299,
+  },
+  {
+    id: "22",
+    name: "Learn Pro Backend - JS",
+    price: 399,
+  },
+  {
+    id: "33",
+    name: "Learn Mongo DB",
+    price: 199,
+  },
+];
+
+app.get("/api/v1/app", (req, res) => {
   res.send("Hello From Punit's App");
 });
+
+// app.get("", (req, res) => {
+//   res.send("");
+// });
 
 app.listen(PORT, () => {
   console.log(`App is tuning on Port ${PORT}`);
